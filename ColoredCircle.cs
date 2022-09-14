0821f6e71ed0ce3d398558290710595d97960295
baddo <baddo@DESKTOP-PU59OLN>
2022-09-14 10:47:40 -0400

    Add project files.

---
+++ ColoredCircle.cs
@@ -0,0 +1,59 @@
+﻿/**
+ Josh Alley
+ 9/14/22
+ 
+ This a program that takes user input of radius and color
+ and out puts a message with radius, color, area, and circumference.
+
+    |===========================================|
+    |            ColoredCircle                  |
+    |-------------------------------------------|
+    | - radius : double                         |
+    | - color  : string                         |
+    |-------------------------------------------|
+    | + SetRadius(radiusParameter: double): void|
+    | + GetRadius(): double                     |
+    | + SetColor(colorOfCircle: string): void   |
+    | + GetColor(): string                      |
+    | + ComputeArea(): int                      |
+    | + ComputeCircumference(): int             |
+    |===========================================|
+**/
+
+
+
+class ColoredCircle
+{
+    private double radius;
+    private string color;
+
+    public void SetRadius(double radiusParameter)
+    {
+        radius = radiusParameter;
+    }
+
+    public double GetRadius()
+    {
+        return radius;
+    }
+
+    public void SetColor(string colorOfCircle)
+    {
+        color = colorOfCircle;
+    }
+
+    public string GetColor()
+    {
+        return color;
+    }
+
+    public int ComputeArea()
+    {
+        return (int)(3.14159 * radius * radius);
+    }
+
+    public int ComputeCircumference()
+    {
+        return (int)(3.14159 * radius * 2);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ColoredCircle myColoerdCircle = new ColoredCircle();
+
+        Console.WriteLine("Welcome to Colored Circle");
+
+        Console.WriteLine("Please enter your Circle Radius as a int");
+        string userInputRadius = Console.ReadLine();
+        double radius = double.Parse(userInputRadius);
+
+        Console.WriteLine("Please enter your Circle Color as a String");
+        string userInputColor = Console.ReadLine();
+        string color = userInputColor;
+
+        myColoerdCircle.SetRadius(radius);
+
+        myColoerdCircle.SetColor(color);
+
+        Console.WriteLine($"Your radius is {myColoerdCircle.GetRadius()}" + $" and the color is {myColoerdCircle.GetColor()}." + $" The circumference of the circle is {myColoerdCircle.ComputeCircumference()}" + $" and the area is {myColoerdCircle.ComputeArea()}.");
+
+    }
+}
